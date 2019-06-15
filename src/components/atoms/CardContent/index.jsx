bedfc36e354b050d1b20e273e07ae0d69e9e4f76
Sergio C. Orozco Torres <scot3004@gmail.com>
2019-06-14 22:41:16 -0500

    fix(card): Word-wrap in card content
    fix #28

--- src/components/atoms/CardContent/index.jsx
+++ src/components/atoms/CardContent/index.jsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 export const CardContent = styled.div`
   flex: 1;
+  hyphens: auto;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
 `;
 
 export default CardContent;
